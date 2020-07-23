c162c97b8897e81b2741ef7f8e47464ac20cfe09
JulieDjidji <25065691+JulieDjidji@users.noreply.github.com>
2020-07-23 14:17:56 +0200

    Add files via upload

---
+++ examples/plot_delimitationOfBuildingAreas.py
@@ -0,0 +1,54 @@
+"""
+Délimitation de tâches bâties
+----------------------------------------------------------------
+Dans ce notebook, nous allons délimiter des zones bâties pour la ville de Caen à partir des données de bâti de la Base de données topographique 
+"""
+# sphinx_gallery_thumbnail_number = 3
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import geopandas as gpd
+import wget
+from zipfile import ZipFile
+
+
+###############################################################################
+# Récupération et lecture des données
+# ================================
+#
+# On récupère les données stockées sur un serveur de stockage (préciser où les récupérer)
+url="https://minio.lab.sspcloud.fr/ev43ru/autour_de_caen_2154.zip"
+file = url.split('/')[-1]
+wget.download(url, file)
+with ZipFile(file, 'r') as files: 
+    files.extractall()
+    
+###############################################################################
+# On ouvre le fond du bâti de la ville de Caen 
+immeuble = gpd.read_file('autour_de_caen_2154.shp')
+
+###############################################################################
+# Délimitation des zones bâties
+# ================================
+#
+# On calcule un buffer de 100 mètres autour de chaque bâtiment
+
+immeuble['geometry']=immeuble.geometry.buffer(100)
+
+###############################################################################
+# On fusionne les buffers avec une intersection non nulle et on affiche les zones fusionnées
+
+union_buffer=gpd.GeoDataFrame({'geometry':immeuble.unary_union})
+union_buffer.plot()
+
+###############################################################################
+# On effectue un buffer négatif pour affiner la délimitation afin de compenser le buffer positif prélablement réaliser pour capter les relations de bâtiments voisins. L'affichage de la carte montre des zones plus délimitées.
+
+union_buffer['zone']=union_buffer.buffer(-100)
+union_buffer['zone'].plot()
+
+###############################################################################
+# On décide d'afficher que les zones de plus de 8 hectares.
+
+union_buffer['aire']=union_buffer['zone'].area/10000
+union_buffer[union_buffer.aire>8].plot()
