792c35e534efce88541762afe7300d037cbec0e9
JulieDjidji <25065691+JulieDjidji@users.noreply.github.com>
2020-07-23 16:58:25 +0200

    Update config.py

--- examples/config.py
+++ examples/config.py
@@ -1,4 +1,6 @@
 # Pour fusionner le .env avec les variables d'environnement
 
+import os
 from dotenv import load_dotenv
+
 load_dotenv(dotenv_path=os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
