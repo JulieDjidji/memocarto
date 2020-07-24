314b7e094466886d9d8124d1a483e3edec96d0c8
JulieDjidji <25065691+JulieDjidji@users.noreply.github.com>
2020-07-24 09:00:52 +0200

    Update plot_readWrite.py

--- examples/plot_readWrite.py
+++ examples/plot_readWrite.py
@@ -38,7 +38,7 @@ pop_com = commune.merge(population, left_on='code', right_on='DEPCOM')
 # On peut afficher une carte de densité en quelques lignes de code en recourant à la fonction ``gpd.plot()``. 
 
 pop_com['densite'] = pop_com.PTOT / pop_com.geometry.area * 1000000 
-pop_com.plot('densite',legend=True, scheme='quantiles', cmap='OrRd', figsize=(10,10))
+ax = pop_com.plot('densite',legend=True, scheme='quantiles', cmap='OrRd', figsize=(10,10))
 
 ###############################################################################
 # Aggregation des données pour produire des données départementales et régionales
@@ -52,7 +52,7 @@ pop_com.plot('densite',legend=True, scheme='quantiles', cmap='OrRd', figsize=(10
 
 pop_dep = pop_com.dissolve(by='dep', aggfunc='sum', as_index=False)
 pop_dep['densite'] = pop_dep.PTOT / pop_dep.geometry.area * 1000000
-pop_dep.plot('densite',legend=True, scheme='quantiles', figsize=(10,10),cmap='OrRd')
+ax = pop_dep.plot('densite',legend=True, scheme='quantiles', figsize=(10,10),cmap='OrRd')
 
 ###############################################################################
 # Carte au niveau régional
@@ -60,4 +60,4 @@ pop_dep.plot('densite',legend=True, scheme='quantiles', figsize=(10,10),cmap='Or
 
 pop_reg = pop_com.dissolve(by='reg', aggfunc='sum', as_index=False)
 pop_reg['densite'] = pop_reg.PTOT / pop_reg.geometry.area * 1000000
-pop_reg.plot('densite',legend=True, scheme='quantiles', figsize=(10,10),cmap='OrRd')
+ax = pop_reg.plot('densite',legend=True, scheme='quantiles', figsize=(10,10),cmap='OrRd')
