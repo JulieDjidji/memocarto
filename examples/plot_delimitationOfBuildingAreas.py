6696d338b875e48d3b0cdeb53acc70927a18b995
JulieDjidji <25065691+JulieDjidji@users.noreply.github.com>
2020-08-04 14:14:21 +0200

    Update plot_delimitationOfBuildingAreas.py

--- examples/plot_delimitationOfBuildingAreas.py
+++ examples/plot_delimitationOfBuildingAreas.py
@@ -1,7 +1,7 @@
 """
 Délimitation de tâches bâties
 ----------------------------------------------------------------
-Dans ce notebook, nous allons délimiter des zones bâties pour la ville de Caen à partir des données de bâti de la Base de données topographique (BD TOPO).
+Dans ce notebook, nous allons délimiter des zones bâties pour la ville de Caen.
 """
 # sphinx_gallery_thumbnail_number = 3
 import os
@@ -24,7 +24,8 @@ bati = gpd.read_file(os.getenv("URL_BATI_VILLE"))
 #
 # On calcule un buffer de 100 mètres autour de chaque bâtiment.
 
-bati['geometry']=bati.geometry.buffer(100)
+bati_buffer_positif=bati.copy()
+bati_buffer_positif['geometry']=bati_buffer_positif.geometry.buffer(100)
 
 ###############################################################################
 # On fusionne les buffers avec une intersection non nulle et on affiche les zones fusionnées.
@@ -35,11 +36,12 @@ ax = union_buffer.plot()
 ###############################################################################
 # On effectue un buffer négatif pour affiner la délimitation afin de compenser le buffer positif prélablement réaliser pour capter les relations de bâtiments voisins. L'affichage de la carte montre des zones plus délimitées.
 
-union_buffer['zone']=union_buffer.buffer(-100)
-ax = union_buffer['zone'].plot()
+union_buffer_negatif=union_buffer.copy()
+union_buffer_negatif['geometry']=union_buffer_negatif.buffer(-100)
+ax = union_buffer_negatif['geometry'].plot()
 
 ###############################################################################
 # On décide d'afficher que les zones de plus de 8 hectares.
 
-union_buffer['aire']=union_buffer['zone'].area/10000
-ax = union_buffer[union_buffer.aire>8].plot()
+union_buffer_negatif['aire']=union_buffer_negatif['geometry'].area/10000
+ax = union_buffer_negatif[union_buffer_negatif.aire>8].plot()
