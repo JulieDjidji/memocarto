6f42572f3ed265711cb70142de69abb8a31b4610
JulieDjidji <25065691+JulieDjidji@users.noreply.github.com>
2020-08-11 09:49:05 +0200

    Update plot_delimitationOfBuildingAreas.py

--- examples/plot_delimitationOfBuildingAreas.py
+++ examples/plot_delimitationOfBuildingAreas.py
@@ -43,5 +43,5 @@ ax = union_buffer_negatif['geometry'].plot()
 ###############################################################################
 # On décide d'afficher que les zones de plus de 8 hectares.
 
-#union_buffer_negatif['aire']=union_buffer_negatif['geometry'].area/10000
+union_buffer_negatif['aire']=union_buffer_negatif['geometry'].area/10000
 #ax = union_buffer_negatif[union_buffer_negatif.aire>8].plot()
